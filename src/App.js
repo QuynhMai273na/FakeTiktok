adf75f91ec4f3e28f7c30abc9b542b02eefabb1e
Nguyễn Thị Quỳnh Mai <147372843+QuynhMai273na@users.noreply.github.com>
2024-11-23 17:44:07 +0700

    Question 1-2-3

--- src/App.js
+++ src/App.js
@@ -1,60 +1,70 @@
-import React, { useEffect, useState, useRef } from 'react';
-import './App.css';
-import VideoCard from './components/VideoCard';
-import BottomNavbar from './components/BottomNavbar';
-import TopNavbar from './components/TopNavbar';
 
-// This array holds information about different videos
+import React, { useEffect, useState, useRef } from "react";
+import "./App.css";
+import VideoCard from "./components/VideoCard";
+import BottomNavbar from "./components/BottomNavbar";
+import TopNavbar from "./components/TopNavbar";
+
+// Array holding video details
 const videoUrls = [
-  {
-    url: require('./videos/video1.mp4'),
-    profilePic: 'https://p16-sign-useast2a.tiktokcdn.com/tos-useast2a-avt-0068-giso/9d429ac49d6d18de6ebd2a3fb1f39269~c5_100x100.jpeg?x-expires=1688479200&x-signature=pjH5pwSS8Sg1dJqbB1GdCLXH6ew%3D',
-    username: 'csjackie',
-    description: 'Lol nvm #compsci #chatgpt #ai #openai #techtok',
-    song: 'Original sound - Famed Flames',
-    likes: 430,
-    comments: 13,
-    saves: 23,
-    shares: 1,
-  },
-  {
-    url: require('./videos/video2.mp4'),
-    profilePic: 'https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/eace3ee69abac57c39178451800db9d5~c5_100x100.jpeg?x-expires=1688479200&x-signature=wAkVmwL7lej15%2B16ypSWQOqTP8s%3D',
-    username: 'dailydotdev',
-    description: 'Every developer brain @francesco.ciulla #developerjokes #programming #programminghumor #programmingmemes',
-    song: 'tarawarolin wants you to know this isnt my sound - Chaplain J Rob',
-    likes: '13.4K',
-    comments: 3121,
-    saves: 254,
-    shares: 420,
-  },
-  {
-    url: require('./videos/video3.mp4'),
-    profilePic: 'https://p77-sign-va.tiktokcdn.com/tos-maliva-avt-0068/4e6698b235eadcd5d989a665704daf68~c5_100x100.jpeg?x-expires=1688479200&x-signature=wkwHDKfNuIDqIVHNm29%2FRf40R3w%3D',
-    username: 'wojciechtrefon',
-    description: '#programming #softwareengineer #vscode #programmerhumor #programmingmemes',
-    song: 'help so many people are using my sound - Ezra',
-    likes: 5438,
-    comments: 238,
-    saves: 12,
-    shares: 117,
-  },
-  {
-    url: require('./videos/video4.mp4'),
-    profilePic: 'https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/4bda52cf3ad31c728153859262c329db~c5_100x100.jpeg?x-expires=1688486400&x-signature=ssUbbCpZFJj6uj33D%2BgtcqxMvgQ%3D',
-    username: 'faruktutkus',
-    description: 'Wait for the end | Im RTX 4090 TI | #softwareengineer #softwareengineer #coding #codinglife #codingmemes ',
-    song: 'orijinal ses - Computer Science',
-    likes: 9689,
-    comments: 230,
-    saves: 1037,
-    shares: 967,
-  },
-];
+    {
+      url: require("./videos/video1.mp4"),
+      profilePic:
+        "https://p16-sign-useast2a.tiktokcdn.com/tos-useast2a-avt-0068-giso/9d429ac49d6d18de6ebd2a3fb1f39269~c5_100x100.jpeg?x-expires=1688479200&x-signature=pjH5pwSS8Sg1dJqbB1GdCLXH6ew%3D",
+      username: "csjackie",
+      description: "Lol nvm #compsci #chatgpt #ai #openai #techtok",
+      song: "Original sound - Famed Flames",
+      likes: 430,
+      comments: 13,
+      saves: 23,
+      shares: 1,
+    },
+    {
+      url: require("./videos/video2.mp4"),
+      profilePic:
+        "https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/eace3ee69abac57c39178451800db9d5~c5_100x100.jpeg?x-expires=1688479200&x-signature=wAkVmwL7lej15%2B16ypSWQOqTP8s%3D",
+      username: "dailydotdev",
+      description:
+        "Every developer brain @francesco.ciulla #developerjokes #programming #programminghumor #programmingmemes",
+      song: "tarawarolin wants you to know this isnt my sound - Chaplain J Rob",
+      likes: "13.4K",
+      comments: 3121,
+      saves: 254,
+      shares: 420,
+    },
+    {
+      url: require("./videos/video3.mp4"),
+      profilePic:
+        "https://p77-sign-va.tiktokcdn.com/tos-maliva-avt-0068/4e6698b235eadcd5d989a665704daf68~c5_100x100.jpeg?x-expires=1688479200&x-signature=wkwHDKfNuIDqIVHNm29%2FRf40R3w%3D",
+      username: "wojciechtrefon",
+      description:
+        "#programming #softwareengineer #vscode #programmerhumor #programmingmemes",
+      song: "help so many people are using my sound - Ezra",
+      likes: 5438,
+      comments: 238,
+      saves: 12,
+      shares: 117,
+    },
+    {
+      url: require("./videos/video4.mp4"),
+      profilePic:
+        "https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/4bda52cf3ad31c728153859262c329db~c5_100x100.jpeg?x-expires=1688486400&x-signature=ssUbbCpZFJj6uj33D%2BgtcqxMvgQ%3D",
+      username: "faruktutkus",
+      description:
+        "Wait for the end | Im RTX 4090 TI | #softwareengineer #softwareengineer #coding #codinglife #codingmemes ",
+      song: "orijinal ses - Computer Science",
+      likes: 9689,
+      comments: 230,
+      saves: 1037,
+      shares: 967,
+    },
+  ];
 
 function App() {
   const [videos, setVideos] = useState([]);
   const videoRefs = useRef([]);
+  const [currentIndex, setCurrentIndex] = useState(0); // To track the current video index
+  const startY = useRef(null); // To track initial Y position of mouse
 
   useEffect(() => {
     setVideos(videoUrls);
@@ -63,46 +73,96 @@ function App() {
   useEffect(() => {
     const observerOptions = {
       root: null,
-      rootMargin: '0px',
-      threshold: 0.8, // Adjust this value to change the scroll trigger point
+      rootMargin: "0px",
+      threshold: 0.8,
     };
-
+    
     // This function handles the intersection of videos
+    // It is triggered when the video enters or leaves the viewport
     const handleIntersection = (entries) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
           const videoElement = entry.target;
-          videoElement.play();
+          videoElement
+            .play()
+            .catch((error) => console.error("Error: ", error));
         } else {
-          const videoElement = entry.target;
-          videoElement.pause();
+          entry.target.pause();
         }
       });
     };
 
-    const observer = new IntersectionObserver(handleIntersection, observerOptions);
+    const observer = new IntersectionObserver(
+      handleIntersection,
+      observerOptions
+    );
 
-    // We observe each video reference to trigger play/pause
-    videoRefs.current.forEach((videoRef) => {
-      observer.observe(videoRef);
-    });
-
+// We observe each video reference to trigger play/pause
+videoRefs.current.forEach((videoRef) => {
+  observer.observe(videoRef);
+});
     // We disconnect the observer when the component is unmounted
+
     return () => {
       observer.disconnect();
     };
   }, [videos]);
 
-  // This function handles the reference of each video
+    // This function handles the reference of each video
   const handleVideoRef = (index) => (ref) => {
     videoRefs.current[index] = ref;
   };
 
+  const handleNextVideo = () => {
+    if (currentIndex < videos.length - 1) {
+      setCurrentIndex((prevIndex) => prevIndex + 1); // Chuyển sang video tiếp theo
+    }
+  };
+  
+  const handlePreviousVideo = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex((prevIndex) => prevIndex - 1); // Quay lại video trước
+    }
+  };
+
+  const handleMouseDown = (event) => {
+    startY.current = event.clientY; // Lưu vị trí Y khi bắt đầu kéo
+  };
+
+  const handleMouseUp = (event) => {
+    const threshold = 100;
+    if (startY.current === null) return;
+
+    const deltaY = event.clientY - startY.current; // Tính khoảng cách kéo
+    startY.current = null; // Reset vị trí
+
+    if (Math.abs(deltaY) > threshold) {
+      if (deltaY > 0) {
+        // Kéo xuống
+        handlePreviousVideo();
+      } else {
+        // Kéo lên
+        handleNextVideo();
+      }
+    }
+  };
+
+  useEffect(() => {
+    // Scroll to the current video
+    const currentVideo = videoRefs.current[currentIndex];
+    if (currentVideo) {
+      currentVideo.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [currentIndex]);
+
   return (
-    <div className="app">
+    <div
+      className="app"
+      onMouseDown={handleMouseDown}
+      onMouseUp={handleMouseUp}
+    >
       <div className="container">
         <TopNavbar className="top-navbar" />
-        {/* Here we map over the videos array and create VideoCard components */}
         {videos.map((video, index) => (
           <VideoCard
             key={index}
@@ -114,16 +174,16 @@ function App() {
             comments={video.comments}
             shares={video.shares}
             url={video.url}
-            profilePic={video.profilePic}
+            // profilePic={video.profilePic}
+            profilePic="https://static1.dienanh.net/upload/202106/2d7fa6ab-1f58-41a3-b249-910d8baa1e88.jpg"
             setVideoRef={handleVideoRef(index)}
-            autoplay={index === 0}
+            autoplay={index === currentIndex}
           />
         ))}
         <BottomNavbar className="bottom-navbar" />
       </div>
     </div>
   );
-  
 }
 
 export default App;
--- src/components/FooterRight.jsx
+++ src/components/FooterRight.jsx
@@ -1,12 +1,115 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCirclePlus, faCircleCheck, faHeart, faCommentDots, faBookmark, faShare } from '@fortawesome/free-solid-svg-icons';
+import { faCirclePlus, faCircleCheck, faHeart, faCommentDots, faBookmark, faShare, faVolumeLow, faVolumeMute } from '@fortawesome/free-solid-svg-icons';
 import './FooterRight.css';
 
-function FooterRight({ likes, comments, saves, shares, profilePic }) {
+function FooterRight({ likes, comments, saves, shares, profilePic, videoRef }) {
+  const [liked, setLiked]=useState(false);
+  const [saved, setSaved]= useState(false);
+  const [muted, setMuted] = useState(false); // Trạng thái âm thanh
+  const [userAddIcon, setUserAddIcon]=useState(faCirclePlus);
+
+  const handleUserAddClick = () =>{
+    setUserAddIcon(faCircleCheck);
+    setTimeout(()=>{
+      setUserAddIcon(null);
+    }, 3000);
+  };
+
+  const parseLikesCount = (count) =>{
+    if(typeof count === 'string'){
+      if(count.endsWith('K')){
+        return parseFloat(count)*1000;
+      }
+      return parseInt(count);
+    }
+    return count;
+  };
+
+  const formatLikesCount = (count) => {
+    if(count >= 1000){
+      return (count/1000).toFixed(1)+'K';
+    }
+    return count;
+  };
+
+  const handleLikeClick = () =>{
+    setLiked((prevLiked)=> !prevLiked);
+  }
+
+  const handleMuteClick = () => {
+    if (videoRef && videoRef.current) {
+      const isMuted = !videoRef.current.muted;
+      videoRef.current.muted = isMuted; // Thay đổi trạng thái âm thanh của video
+      setMuted(isMuted); // Cập nhật trạng thái `muted`
+    }
+  };
 
   return (
+    <div className='footer-right'>
+      <div className='sidebar-icon'>
+        {/* the user profile picture */}
+        {profilePic ? (
+          // display user profile picture
+          <img src = {profilePic} alt='Profile' className='userprofile' style={{width: '45px', height:'45px',color: '#616161'}} />
+        ) : null}
+        {/* user add icon */}
+        <FontAwesomeIcon icon={userAddIcon} className='useradd' style={{width: '15px', height:'15px',color: '#FF0000'}} onClick={handleUserAddClick} />
+      </div>
 
+      <div className='sidebar-icon'>
+        {/* like icon */}
+        <FontAwesomeIcon icon={faHeart} style={{width: '35px', height:'35px',color: liked ? '#FF0000' : 'white'}} onClick={handleLikeClick} />
+
+        {/* display the formatted likes count */}
+        <p>{formatLikesCount(parseLikesCount(likes) +(liked ? 1 : 0))}</p>
+      </div>
+
+      <div className='sidebar-icon'>
+        {/* the comment icon */}
+        <FontAwesomeIcon icon={faCommentDots} style={{width: '35px', height:'35px',color: 'white'}} />
+
+        {/* display the comments count */}
+        <p>{comments}</p>
+      </div>
+
+      <div className='sidebar-icon'>
+        {/* the save icon */}
+        {saved ? (
+          //display bookmark icon when saved
+          <FontAwesomeIcon icon = {faBookmark} style={{width: '35px', height:'35px',color: '#ffc107'}} onClick={()=>setSaved(false)} />
+        ) : (
+          // display bookmark icon when not saved
+          <FontAwesomeIcon icon = {faBookmark} style={{width: '35px', height:'35px',color: 'white'}} onClick={()=>setSaved(true)} />
+        )}
+        {/* display the number of saves */}
+        <p>{saved ? saves + 1 : saves}</p>
+      </div>
+
+      <div className='sidebar-icon'>
+        {/* the share icon */}
+        <FontAwesomeIcon icon = {faShare} style={{width: '35px', height:'35px',color: 'white'}} />
+
+        {/* display the number of shares */}
+        <p>{shares}</p>
+      </div>
+
+      <div className='sidebar-icon mute'>
+        {/* Icon thay đổi dựa trên trạng thái muted */}
+        <FontAwesomeIcon
+          icon={muted ? faVolumeMute : faVolumeLow}
+          style={{ width: '35px', height: '35px', color: 'white' }}
+          onClick={handleMuteClick}
+        />
+      </div>
+
+      <div className='sidebar-icon record'>
+        {/* the record icon */}
+        <img src="https://static.thenounproject.com/png/934821-200.png" alt="Record icon" />
+      </div>
+      
+
+    </div>
   );
 }
 
